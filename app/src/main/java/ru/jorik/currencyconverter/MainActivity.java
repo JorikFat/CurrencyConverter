36a3bb5e148ee9ff53d535de99e675cc0c16f2d5
Jorik <RATlius@mail.ru>
2017-08-12 23:54:24 +0300

    cleaning code

--- app/src/main/java/ru/jorik/currencyconverter/MainActivity.java
+++ app/src/main/java/ru/jorik/currencyconverter/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
@@ -56,32 +54,6 @@ public class MainActivity extends AppCompatActivity {
             new AsyncUpdateDB().execute(this);
         }
 
-
-//        try {
-//            serverResponce = new AsyncRequest().execute(this).get();
-//        } catch (InterruptedException | ExecutionException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//        serverResponce = serverResponce.replace(',', '.');
-//
-//        Reader reader = new StringReader(serverResponce);
-//        Persister deserializer = new Persister();
-//
-//        try {
-//            valCurs = deserializer.read(ValCurs.class, reader, false);
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//        //добавление рубля, так как его нету в ответе сервера
-//        Valute rubValute = new Valute();
-//        rubValute.charCode = "RUB";
-//        rubValute.name = "Российский рубль";
-//        rubValute.nominal = 1;
-//        rubValute.value = 1;
-//        valCurs.valuteList.add(0, rubValute);
-
         String[] valuteArray;
         valuteArray = getValuteArray();
         final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, valuteArray);
@@ -91,31 +63,6 @@ public class MainActivity extends AppCompatActivity {
         spinner2 = ((Spinner)findViewById(R.id.spinner2));
         spinner1.setAdapter(arrayAdapter);
         spinner2.setAdapter(arrayAdapter);
-
-
-
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private String[] getValuteArray(){
@@ -154,8 +101,6 @@ public class MainActivity extends AppCompatActivity {
         double result = tempValue / coef2;
 
         ((TextView)findViewById(R.id.textView)).setText(String.format("%.4f",result));
-
-
     }
 
 
